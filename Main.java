808f378668cf28c4a8fb82c584b1e9eaa8e78cde
Micho0o <162017857+Micho0o@users.noreply.github.com>
2024-05-04 12:47:27 +0200

    zad4

--- Main.java
+++ Main.java
@@ -7,23 +7,31 @@ import Company.Utils.MathUtils;
 public class Main 
 {
 
-    public static void main(String[] args) 
+    public static void main(String[] args)
     {
-        try 
-        {
-            Person person = new Person("Michał", 222);
-            System.out.println("Imie " + person.getName());
-            System.out.println("Wiek " + person.getAge());
-        } 
-        catch (InvalidAgeException e) 
-        {
-            System.out.println(e.getMessage());
-        }
-
-        int result = MathUtils.add(55, 555);
-        System.out.println("Wynik dodawania " + result);
+     Person[] people = new Person[5];
         
-        Messenger Messenger = new EmailMessenger();
-        Messenger.sendMessage("Wynik z dodawania wyslany mailem: " + result);
+     final int b = 10;
+
+     try 
+     {
+         people[0] = new Person("John Doe", 30);
+         people[1] = new Person("Krystian Dda", 55);
+         people[2] = new Person("Bartek Kowalski", 11);
+         people[3] = new Person("EMil Dudek", 23);
+         people[4] = new Person("Michał Lipski", 87);
+
+         for (Person person : people) 
+         {
+             int result = MathUtils.add(person.getAge(), b);
+             System.out.println("Imie " + person.getName() + " Wiek: " + result);
+
+             Messenger messenger = new EmailMessenger();
+             messenger.sendMessage(person.getName() + ". Jego wiek wyslany mailem " + result);
+         }
+     } catch (InvalidAgeException e) 
+     {
+         System.out.println(e.getMessage());
+     }
     }
 }
